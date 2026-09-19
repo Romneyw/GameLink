042cd56dc588734c3103172f0f0d99ff34129d30
Romneyw <w2071480@gmail.com>
2026-09-19 11:16:11 +0200

    Initialize Supabase client in script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://arlhkjocegnppfziikpo.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_aWWQyhe1KrXskToB9nU2_A_SsFpUPbv";
 
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
+
+console.log("GameLink connected to Supabase!");
